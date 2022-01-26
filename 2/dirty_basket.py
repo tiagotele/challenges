1dc7f0b20c4c40e86febca14b40919d32af40808
Tiago <tiagotele@gmail.com>
2022-01-26 08:56:37 -0300

    TODO fix clean basket behaviour

--- 2/dirty_basket.py
+++ 2/dirty_basket.py
@@ -1,11 +1,19 @@
 import threading
 
 class Basket:
-    def __init__(self, fruits=0) -> None:
+    def __init__(self, fruits=0, name='') -> None:
         self.fruits=fruits
+        self.will_have_more_fruits=True
         self._lock = threading.Lock()
+        self.name=name
     
     def put_fruit(self):
         with self._lock:
             self.fruits += 1
-            print(f"Basket has {self.fruits} fruits.")
+            print(f"Basket {self.name} has {self.fruits} fruits.")
+    
+    def get_fruit(self):
+        if self.fruits > 0:
+            with self._lock:
+                self.fruits -= 1
+    
--- 2/run.py
+++ 2/run.py
@@ -5,15 +5,23 @@ import concurrent.futures
 
 
 if __name__ == "__main__":
-    tree=Tree()
-    dirty_basket=Basket()
+    tree=Tree(10)
+    dirty_basket=Basket(name='dirty')
+    clean_basket=Basket(name='clean')
 
     farmers=[]
     for index in range(3):
-        farmers.append(Farmer(index,tree,dirty_basket))
+        farmers.append(Farmer(index,tree,dirty_basket=dirty_basket, clean_basket=None))
+    cleaners=[]
+    for index in range(3):
+        cleaners.append(Farmer(index,None,dirty_basket=dirty_basket, clean_basket=clean_basket))
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        farmers_future = [ executor.submit(f.get_fruit) for f in farmers ]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as clean_executors:
+        cleaners_future = [ clean_executors.submit(c.get_fruit_from_dirty_basket) for c in cleaners ]
+    
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as dirty_executor:
+            farmers_future = [ dirty_executor.submit(f.get_fruit_from_tree) for f in farmers ]
 
 
     print("end all")
+    print(f"clean basket total={clean_basket.get_fruit()}")
