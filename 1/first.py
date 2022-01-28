2014f4e7b2efed5619c6770c9acffb9cff44f643
Tiago <tiagotele@gmail.com>
2022-01-28 10:33:26 -0300

    Adding small description about how to run.

--- 1/first.py
+++ 1/first.py
@@ -1,21 +1,25 @@
 from os import listdir
 from os.path import isfile
+import sys
 
 
-def my_func(suffix, path):
+def get_files_only(suffix, path):
     final_files=[]
 
     for f in listdir(path):
         if isfile(path+f):
             final_files.append(path+f)
         else:
-            final_files.extend(my_func(suffix, path+f+"/"))
+            final_files.extend(get_files_only(suffix, path+f+"/"))
     return [f for f in final_files if f.endswith(suffix)]
 
 if __name__ == '__main__':
-    mypath="/Users/Tiago/git/cocus/"
-    first_level=mypath+"1/first_level_files/"
+    if len(sys.argv) != 3:
+        print("Missing arguments")
 
-    final_files=my_func(".pdf", mypath+"1/")
+    suffix = sys.argv[1]
+    path = sys.argv[2]
+
+    final_files=get_files_only(sys.argv[1], sys.argv[2])
     for f in final_files:
         print(f)
